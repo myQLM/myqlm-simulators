9b3b4364ab90a995f86859dd838bb3373b7c7ec5
a650719 <bertrand.marchand@atos.net>
2019-02-01 18:15:09 +0100

    started adding tests

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,18 +1,24 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
+import os
 import unittest
 
+from qat.core.task import Task
+from qat.core.circ import readcirc
+from qat.pylinalg import get_qpu_server
 
-class TestMyFunction(unittest.TestCase):
+BSM_TESTDIR = os.getenv("TESTS_DIR")
 
-    def test_feature_1(self):
-        a = 1
-        b = 1
-        self.assertTrue(a == b)
+class TestSimpleCircExec(unittest.TestCase):
 
-    def a_method(self):
-        print("Hello")
+    def test_default_mode(self):
+   
+        fname = os.path.join(BSM_TESTDIR, "bellstate.circ") 
+        circuit = readcirc(fname)
+
+        task = Task(circuit, get_qpu_server())
+
+        result = task.execute()
+
+        self.assertTrue(result.state[0]==result.state[1])
 
 
 if __name__ == '__main__':

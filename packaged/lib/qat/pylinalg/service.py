3b8ca8322ff2b41bb3f0693cc1f5f200d8415744
a650719 <bertrand.marchand@atos.net>
2019-02-12 11:37:08 +0100

    added state analyzer + default mode more correct (subset of qubits)

--- packaged/lib/qat/pylinalg/service.py
+++ packaged/lib/qat/pylinalg/service.py
@@ -61,47 +61,98 @@ class PyLinalg(qproc.Plugin):
         logging.info("New simulation")
 
         qproc_state_vec = simu_types.StateVector()
+        qproc_state_vec.states = [] # container: stores final states
+                                    # in both DEFAULT and ANALYZE
             
         circ = simu_input.task.circuit # shorter
 
         np_state_vec, history = np_engine.simulate(circ) # perform simu
 
+       # if no qubits specified then measure all.
+        if simu_input.options.qbits:
+            meas_qubits = simu_input.options.qbits
+        else:
+            meas_qubits = [k for k in range(circ.nbqbits)]
+
+        # for simpler handling of subset-of-qubits case.
+        all_qubits = False
+        if len(meas_qubits) == circ.nbqbits:
+            all_qubits = True
+
+       # that's a big if. 
         if simu_input.options.mode == simu_types.Mode.ANALYZE:
             logging.info("Analyze mode")
 
-            print("sucessfully called submit in analyze mode")
+            if simu_input.options.range.last_state:
+                return qproc_state_vec
 
-            # Implementation goes here.
-            print(np_state_vec)
+            # trace over un-measured qubits
+            if not all_qubits:
+            
+                all_qb = range(circ.nbqbits) # shorter
+                sum_axes = tuple(qb for qb in all_qb if qb not in meas_qubits)
+
+                # state_vec is transformed into vector of PROBABILITIES
+                np_state_vec = np.abs(np_state_vec**2) 
+                np_state_vec = np_state_vec.sum(axis=sum_axes) 
+
+            # setting up threshold
+            threshold = 0.0
+            if simu_input.options.threshold is not None:
+                threshold = simu_input.options.threshold
+
+            # loop over states. val is amp if all_qubits else prob
+            for int_state, val in enumerate(np_state_vec.ravel()):
+    
+                amplitude = None # in case not all qubits
+                if all_qubits:
+                    
+                    amplitude = datamodel_types.ComplexNumber()
+                    amplitude.re = val.real
+                    amplitude.im = val.imag     
+                    prob = abs(val**2)
+
+                else:
+                    prob = val
+
+                if prob <= threshold:
+                    continue
+
+                # byte conversion
+                int_state = core_simutil.rev_bits(int_state, len(meas_qubits))
+                bytes_state = int_state.to_bytes((int_state.bit_length() // 8) + 1,
+                                                byteorder="little")    
+ 
+                # final result object
+                qpu_result = qpu_types.Result(bytes_state, 
+                                                probability=prob,
+                                                amplitude=amplitude)
+
+                # append
+                qproc_state_vec.states.append(qpu_result)
     
         elif simu_input.options.mode == simu_types.Mode.DEFAULT:
             logging.info("Default mode")
-    
-            # if no qubits specified then measure all.
-            if simu_input.options.qbits:
-                meas_qubits = simu_input.options.qbits
-            else:
-                meas_qubits = [k for k in range(circ.nbqbits)]
 
             # measure: 
             intprob_list = np_engine.measure(np_state_vec, 
                                          meas_qubits,
                                          nb_samples=simu_input.options.run_nb)
 
-            qproc_state_vec.states = [] # container: stores final meas samples.
-
             # convert to good format and put in container.
             for res_int, prob in intprob_list:
 
-                # accessing amplitude of result
-                indices = [] 
-                for k in range(len(meas_qubits)):
-                    indices.append(res_int >> k & 1)
-                indices.reverse()
-     
-                amplitude = datamodel_types.ComplexNumber()
-                amplitude.re = np_state_vec[tuple(indices)].real # access
-                amplitude.im = np_state_vec[tuple(indices)].imag # access
+                amplitude = None # in case not all qubits
+                if all_qubits:
+                    # accessing amplitude of result
+                    indices = [] 
+                    for k in range(len(meas_qubits)):
+                        indices.append(res_int >> k & 1)
+                    indices.reverse()
+         
+                    amplitude = datamodel_types.ComplexNumber()
+                    amplitude.re = np_state_vec[tuple(indices)].real # access
+                    amplitude.im = np_state_vec[tuple(indices)].imag # access
 
                 # byte conversion
                 res_int = core_simutil.rev_bits(res_int, len(meas_qubits))

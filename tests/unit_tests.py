5813f6b074b770e8b568f79e4dc1e39f9caf4a7c
a650719 <bertrand.marchand@atos.net>
2019-02-12 13:38:28 +0100

    added comparison with linalg. seems to work.

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -4,6 +4,9 @@ import unittest
 from qat.core.task import Task
 from qat.core.circ import readcirc
 from qat.pylinalg import get_qpu_server
+from qat.linalg import get_qpu_server as get_linalg_qpu_server
+
+import glob
 
 BSM_TESTDIR = os.getenv("TESTS_DIR")
 CIRC_PATH = os.path.join(BSM_TESTDIR, "circuits")
@@ -21,6 +24,55 @@ class TestSimpleCircExec(unittest.TestCase):
 
         self.assertTrue(result.state[0]==result.state[1])
 
+    def test_analyze_mode(self):
+
+        fname = os.path.join(CIRC_PATH, "bellstate.circ") 
+        circuit = readcirc(fname)
+
+        task = Task(circuit, get_qpu_server())
+       
+        for state in task.states():
+            pass 
+
+class TestLinalgCompare(unittest.TestCase):
+
+    def test_linalg_compare(self):
+
+        condition = os.path.join(CIRC_PATH, "*.circ")
+        fnames = glob.glob(condition)
+
+        circuits = []
+
+        for name in fnames:
+            circ = readcirc(name)
+            no = False
+            if len(circ.ops) > 1000:
+                no = True
+            if circ.nbqbits > 10:
+                no = True
+            for op in circ.ops:
+                if op.type > 0:
+                    no = True
+            if no: #oh no
+                break
+            circuits.append(circ)
+
+        for circ in circuits:
+            task1 = Task(circ, get_qpu_server())
+            task2 = Task(circ, get_linalg_qpu_server())
+
+            res1 = {}           
+            res2 = {}           
+ 
+            for state in task1.states():
+                res1[state.state.int] = state.amplitude
+    
+            for state in task2.states():
+                res2[state.state.int] = state.amplitude
+
+            for st in res1.keys():
+                print(res1[st], res2[st])
+                self.assertAlmostEqual(res1[st], res2[st])
 
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], exit=False)

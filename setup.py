e038b4a568961124b7e52efbd9e3a2ec4d901825
Arnaud Gazda <arnaud.gazda@atos.net>
2020-07-08 18:07:37 +0200

    Remove thrift dependency

--- setup.py
+++ setup.py
@@ -24,8 +24,7 @@ setup(
     license="Atos myQLM EULA",
     packages=find_namespace_packages(include=["qat.*"]),
     test_suite="tests",
-    install_requires=["thrift==0.10", "qat-core>=0.0.6",
-                      "qat-lang>=0.0.6", "numpy"],
+    install_requires=["qat-core>=0.0.6", "qat-lang>=0.0.6", "numpy"],
     tests_require=['pytest'],
     cmdclass = {'test': PyTest},
 )

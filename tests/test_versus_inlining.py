8540c93c0b04c032307e54d1b3c25247ac5ea988
gchen <gary.chen@atos.net>
2020-07-10 17:34:55 -0700

    Added LICENSE file; Made source code compliant with Apache license v2.0

--- tests/test_versus_inlining.py
+++ tests/test_versus_inlining.py
@@ -1,16 +1,24 @@
 # -*- coding : utf-8 -*-
-"""
-@authors Simon Martiel <simon.martiel@atos.net>
-@internal
-@copyright 2017-2019  Bull S.A.S.  -  All rights reserved.
-           This is not Free or Open Source software.
-           Please contact Bull SAS for details about its license.
-           Bull - Rue Jean Jaurès - B.P. 68 - 78340 Les Clayes-sous-Bois
 
-@file qat-mps/tests/test_versus_inlining.py
-@brief
-@namespace qat-mps.tests.test_versus_inlining
 """
+    Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on an
+    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied.  See the License for the
+    specific language governing permissions and limitations
+    under the License.
+"""
+
 import unittest
 import numpy as np
 

51c98f58c9370b9da11b65e7825ad9d4a0152d87
Lambert, Cyprien <cyprien.lambert@atos.net>
2023-08-07 10:29:09 +0200

    [[FIX]] Qaptiva lazy import system fix for myQLM

--- qat/__init__.py
+++ qat/__init__.py
@@ -22,3 +22,6 @@
 # Try to find other packages in other folders (with separate build directory)
 from pkgutil import extend_path
 __path__ = extend_path(__path__, __name__)
+
+from .modules import register_modules
+register_modules()
